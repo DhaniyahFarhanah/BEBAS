93d058eb60ba6cd2d5b5b5c90bd681ed0d317266
uhveahn <thearia27@gmail.com>
2023-02-17 12:46:08 +0800

    added enemy scripts (empty)

--- Assets/Scripts/enemy/holdBreathGhost.cs
+++ Assets/Scripts/enemy/holdBreathGhost.cs
@@ -1,23 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class closeEyesGhost : MonoBehaviour
+public class holdBreathGhost : MonoBehaviour
 {
-
-    public float range = 3; //variable for enemy range (default)
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
-
-
