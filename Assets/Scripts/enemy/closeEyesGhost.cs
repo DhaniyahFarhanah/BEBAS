e1e3291e396a22084f999a532d4a5824fa677415
uhveahn <thearia27@gmail.com>
2023-02-20 11:51:09 +0800

    did the security guard thing i think
    the is trigger thing isnt working tho

--- Assets/Scripts/enemy/closeEyesGhost.cs
+++ Assets/Scripts/enemy/closeEyesGhost.cs
@@ -6,7 +6,12 @@ public class closeEyesGhost : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "closeEyesGhost") //if the player collides with objects with the tag "ghost"
+        if (collision.transform.tag == "closeEyesGhost" && Input.GetKey(KeyCode.LeftShift) == true) //if the player collides with objects with the tag "ghost"
+        {
+            PlayerManager.isGameOver = false; //game is over is true
+        }
+
+        else if (collision.transform.tag == "closeEyesGhost" && Input.GetKey(KeyCode.LeftShift) == false)
         {
             PlayerManager.isGameOver = true; //game is over is true
             gameObject.SetActive(false); //destroys the player object 
