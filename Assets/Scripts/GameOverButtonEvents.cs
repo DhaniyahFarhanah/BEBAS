e6f9f2f46e084b9bfa7c1c66fc47915b4a8bd239
DhaniyahFarhanah <noobygal21@gmail.com>
2023-03-19 17:53:35 +0800

    added game over sprites

---
+++ Assets/Scripts/GameOverButtonEvents.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverButtonEvents : MonoBehaviour
+{
+    [SerializeField] Image hand;
+    [SerializeField] Image word;
+    [SerializeField] Sprite handSelected;
+    [SerializeField] Sprite handNotSelected;
+    [SerializeField] Color selected;
+    [SerializeField] Color notSelected;
+    [SerializeField] AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onPointerEnter()
+    {
+        sound.Play();
+        hand.sprite = handSelected;
+        word.color = selected;
+    }
+    public void onPointerExit()
+    {
+        hand.sprite = handNotSelected;
+        word.color = notSelected;
+    }
+}
