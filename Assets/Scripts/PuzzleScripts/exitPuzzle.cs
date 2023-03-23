b1feda0954c7b15db0bf484a171df4cae083465c
DhaniyahFarhanah <noobygal21@gmail.com>
2023-03-23 22:41:53 +0800

    Polish: Fixed some other shit

--- Assets/Scripts/PuzzleScripts/exitPuzzle.cs
+++ Assets/Scripts/PuzzleScripts/exitPuzzle.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class exitPuzzle : MonoBehaviour
 {
     public GameObject player;
+    public GameObject dialogue;
     public bool canClose;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && gameObject.activeInHierarchy && canClose)
+        if (Input.GetKeyDown(KeyCode.Escape) && gameObject.activeInHierarchy && canClose && !dialogue.activeInHierarchy)
         {
             gameObject.SetActive(false);
             player.SetActive(true);
